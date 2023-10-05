38027230636ed5abb29fd954f47b8dbc14349afd
Vlad-Kudin <wolf_od_ua@yahoo.com>
2023-10-05 16:42:43 +0300

    added useEffect and useCallback for request

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import JokeList from "./components/JokeList";
 import "./App.css";
 
 function App() {
-  // const dummyJokes = [
-  //   {
-  //     id: 1,
-  //     type: "general",
-  //     setup: "What do you call a bee that lives in America?",
-  //     punchline: "A USB.",
-  //   },
-  //   {
-  //     id: 2,
-  //     type: "programming",
-  //     setup: "What's the best thing about a Boolean?",
-  //     punchline: "Even if you're wrong, you're only off by a bit.",
-  //   },
-  // ];
 
   const [jokes, setJokes] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchJokesHandler() {
+  const fetchJokesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -37,7 +23,11 @@ function App() {
       setError(e.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  useEffect(() => {
+    fetchJokesHandler();
+  }, [fetchJokesHandler]);
 
   let content = <p>Jokes not found.</p>;
 
